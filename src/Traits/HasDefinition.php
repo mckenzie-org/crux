01a98fc31ab67c85a3b020a7aa9df8e2414bf425
Akshay Kolte <akshay.kolte@etlok.com>
2023-02-16 10:31:24 -0500

    Definition Path Fix

--- src/Traits/HasDefinition.php
+++ src/Traits/HasDefinition.php
@@ -7,7 +7,7 @@ trait HasDefinition
     public function definition()
     {
         $definition_path = config('crux.definitions_path');
-        $file = base_path($definition_path.$this->table.'.json');
+        $file = base_path($definition_path.'/'.$this->table.'.json');
         if(file_exists($file)) {
             return @json_decode(file_get_contents($file),true);
         }
