5c126fed37eae2ceb07faffec2f70dc63d227aa3
Akshay Kolte <akshay.kolte@etlok.com>
2023-04-28 10:16:49 -0400

    Alternative Definitions: Secure Definitions

--- src/Traits/HasDefinition.php
+++ src/Traits/HasDefinition.php
@@ -7,7 +7,7 @@ trait HasDefinition
     public function definition($id = null)
     {
         $definition_path = config('crux.definitions_path');
-        $def_name = $id ?? $this->table;
+        $def_name = $this->table.($id=== null?'':(':'.$id));
         $file = base_path($definition_path.'/'.$def_name.'.json');
         if(file_exists($file)) {
             return @json_decode(file_get_contents($file),true);
