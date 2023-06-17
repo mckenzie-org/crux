9306a1ee7a75ccc1aebc00a30bd20c1dce05fe14
Akshay Kolte <akshay.kolte@etlok.com>
2023-06-17 19:36:26 -0400

    Allow Duplicates In Pivot

--- src/Http/Controllers/CruxModelController.php
+++ src/Http/Controllers/CruxModelController.php
@@ -555,8 +555,14 @@ class CruxModelController extends CruxBaseController {
                 ]
             ],404);
         }
+        if(isset($validated['id'])) {
+            $pivotRowId = $validated['id'];
+            unset($validated['id']);
+            $obj->$child()->newPivotStatement()->where('id', $pivotRowId)->update($validated);
+        } else {
+            $obj->$child()->updateExistingPivot($child_id,$validated);
+        }
 
-        $obj->$child()->updateExistingPivot($child_id,$validated);
         return response()->json(['status'=>0],200);
 
     }
@@ -580,7 +586,7 @@ class CruxModelController extends CruxBaseController {
         return response()->json(['status'=>0]);
     }
 
-    public function detach($id, $child, $child_id)
+    public function detach($id, $child, $child_id, Request $request)
     {
         $obj = $this->model::find($id);
 
@@ -594,7 +600,14 @@ class CruxModelController extends CruxBaseController {
             ],404);
         }
 
-        $obj->$child()->detach($child_id);
+        if($request->has('withId')) {
+            $pivotId = $request->input('withId');
+            $obj->$child()->wherePivot('id',$pivotId)->detach($child_id);
+        } else {
+            $obj->$child()->detach($child_id);
+        }
+
+
 
 
         return response()->json(['status'=>0]);
